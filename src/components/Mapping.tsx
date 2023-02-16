aa5210f3b076e02db811e775f8bece8284965aab
João Mangueira <jamjs7@gmail.com>
2023-02-16 11:30:48 -0300

    Desistind do projeto, não consigo fazer funcionar

--- src/components/Mapping.tsx
+++ src/components/Mapping.tsx
@@ -4,7 +4,7 @@ import {
   MenuItem,
   Select
 } from "@mui/material";
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 import { Loader } from "google-maps";
 import
   React,
@@ -16,10 +16,12 @@ import
   useState
 } from 'react';
 import { sample, shuffle } from "lodash";
+import { RouteExistsError } from "../errors/route-exists.error";
 import { makeCarIcon, makeMarkerIcon, Map } from "../util/map";
 import { Route } from "../util/models";
 import { getCurrentPosition } from "../util/geolocations";
-import {Navibar} from "./Navibar";
+import { Navibar } from "./Navibar";
+import io from "socket.io-client";
 
 const API_URL = process.env.REACT_APP_API_URL as string;
 const googleMapsLoader = new Loader(process.env.REACT_APP_GOOGLE_API_KEY);
@@ -60,6 +62,12 @@ export function Mapping () {
   const [routes, setRoutes] = useState<Route[]>([]);
   const [routeIdSelected, setRouteIdSelected] = useState<string>("");
   const mapRef = useRef<Map>();
+  const socketIORef = useRef<SocketIOClient.Socket>();
+
+  useEffect(() => {
+   socketIORef.current = io.connect(API_URL);
+   socketIORef.current.on('connect', () => console.log('conectou'));
+  }, []);
 
   useEffect(() => {
     fetch(`${API_URL}/routes`)
@@ -87,17 +95,33 @@ export function Mapping () {
 
     const color = sample(shuffle(colors)) as string;
 
-    mapRef.current?.addRoute(routeIdSelected, {
-      currentMarkerOptions: {
-        position: route?.startPosition,
-        icon: makeCarIcon(color),
-      },
-      endMarkerOptions: {
-        position: route?.endPosition,
-        icon: makeMarkerIcon(color),
-      }
+    try {
+      mapRef.current?.addRoute(routeIdSelected, {
+        currentMarkerOptions: {
+          position: route?.startPosition,
+          icon: makeCarIcon(color),
+        },
+        endMarkerOptions: {
+          position: route?.endPosition,
+          icon: makeMarkerIcon(color),
+        }
 
-    });
+      });
+
+      socketIORef.current?.emit('new-direction', {
+        routeId: routeIdSelected
+      });
+
+    } catch (error) {
+      if (error instanceof RouteExistsError) {
+        // enqueueSnackbar(`${route?.title} já adicionado, espere finalizar`, {
+        //   variant: "error"
+        // });
+        return;
+      }
+      throw error;
+    }
+
   }, [routeIdSelected, routes]);
 
   return (
